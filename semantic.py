d064e43a9ab3fb99bad1c96a1f873930a85a7915
j-omeara <127307095+j-omeara@users.noreply.github.com>
2023-03-09 18:43:47 +0000

    Update semantic.py
    added second code extract

--- semantic.py
+++ semantic.py
@@ -9,3 +9,10 @@ word3 = nlp("banana")
 print(word1.similarity(word2))
 print(word3.similarity(word2))
 print(word3.similarity(word1))
+print ("")
+
+# second code extract
+tokens = nlp('cat apple monkey banana ')
+for token1 in tokens:
+    for token2 in tokens:
+        print(token1.text, token2.text, token1.similarity(token2))
